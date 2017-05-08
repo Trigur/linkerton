3b74672a0cadd4d6b54e0c138ae00d7e3ac53d5c
Trigur <trigur@yandex.ru>
2017-05-08 13:48:23 +0300

    Version 0.4

--- module_info.php
+++ module_info.php
@@ -1,12 +1,16 @@
-<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+<?php 
+
+if (! defined('BASEPATH')) {
+    exit('No direct script access allowed');
+}
 
 $com_info = array(
-	'menu_name'   => 'Linkerton',
-	'description' => 'Выборки материалов сайта',
-	'admin_type'  => 'inside',
-	'window_type' => 'xhr',
+    'menu_name'   => 'Linkerton',
+    'description' => 'Выборки материалов сайта',
+    'admin_type'  => 'inside',
+    'window_type' => 'xhr',
     'w'           => 600,
-	'h'           => 550,
-	'version'     => '0.1',
-	'author'      => 'dev@imagecms.net'
+    'h'           => 550,
+    'version'     => '0.4',
+    'author'      => 'trigur@yandex.ru'
 );
